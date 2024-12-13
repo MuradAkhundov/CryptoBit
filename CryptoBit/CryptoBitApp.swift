549cd32b1c54d98d88b9324e53b514361aece4ac
muradakhundov <axundovvmurad@gmail.com>
2024-12-13 16:07:39 +0400

    Initial Commit

---
+++ CryptoBit/CryptoBitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoBitApp.swift
+//  CryptoBit
+//
+//  Created by Murad Akhundov on 13.12.24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoBitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
